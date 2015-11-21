690682fa1f73e2eb78712ebc49338a6410e79c57
pawa <pawa@pawafuru.com>
2015-11-21 12:56:35 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+AozoraBunko::Tools::Checkerkun - It's new $module
+
+# SYNOPSIS
+
+    use AozoraBunko::Tools::Checkerkun;
+
+# DESCRIPTION
+
+AozoraBunko::Tools::Checkerkun is ...
+
+# LICENSE
+
+Copyright (C) pawa.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+pawa <pawa@pawafuru.com>
---
+++ lib/AozoraBunko/Tools/Checkerkun.pm
@@ -0,0 +1,39 @@
+package AozoraBunko::Tools::Checkerkun;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+AozoraBunko::Tools::Checkerkun - It's new $module
+
+=head1 SYNOPSIS
+
+    use AozoraBunko::Tools::Checkerkun;
+
+=head1 DESCRIPTION
+
+AozoraBunko::Tools::Checkerkun is ...
+
+=head1 LICENSE
+
+Copyright (C) pawa.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+pawa E<lt>pawa@pawafuru.comE<gt>
+
+=cut
+
